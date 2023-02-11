4992491d3aab9a5ebeabf8f76b3d4852c132798e
Severn Lortie <sevysdayoff@gmail.com>
2023-02-10 20:41:27 -0500

    Added ALU div module

---
+++ Alu_Div_32.v
@@ -0,0 +1,77 @@
+module Alu_Div_32(input [31:0] dividend, divisor, input clk, output reg [0:0] done, output reg[5:0] count, output reg [31:0] A, Q
+);
+
+	//reg [31:0] A = 31'b0;
+	reg [31:0] M;
+	reg temp;
+	//reg [31:0] Q;
+	//reg [5:0] count;
+	integer i; 
+	
+	// Make sure inputs are positive
+	initial begin
+		A = 32'b0;
+		M = 32'b0;
+		Q = 32'b0;
+		temp = 0; 
+		done = 0;
+		count = 32;
+	end
+	
+	// Use a sequential method to achieve non-restoring division
+	always @(posedge clk) begin
+	
+		// Adopt wire values on clk edge
+		if (count == 32) begin 
+		
+			M = divisor;
+			Q = dividend;
+			
+			// Make positive
+			//if (M[31] == 1) begin 
+			//	M = ~M + 1;
+			//end
+			//if (Q[31] == 1) begin 
+			//	Q = ~Q + 1;
+			//end
+			count = count -1;
+		end else begin
+			// On each clock edge, perform a step in the 
+			// non-restoring algo
+			
+			// Algo. keeps going
+			if (count > 0) begin
+			
+				// Step 1: Shift A and Q left
+				temp = Q[31];
+				$display(A);
+				A = A << 1;
+				$display(A);
+				Q = Q << 1;
+				A[0] = temp; 
+				
+				
+				//If A > 0, sub M from A
+				if (A[31] == 0) begin
+					A = A + (~M + 1);
+				end else begin
+					//$display(A);
+					A = A + M; // Add M to A
+					//$display(A);
+				end
+
+				Q[0] = !A[31];
+				
+				count = count - 1;
+				
+			end else begin
+			
+				// Step 2
+				done = 1;
+		
+			end
+		end
+	end
+	
+
+endmodule
---
+++ alu_div_tb.v
@@ -0,0 +1,20 @@
+`timescale 1ns/10ps
+module alu_div_tb;
+
+	wire [31:0] A, Q, dividend, divisor;
+	assign dividend = 32'b11001100;
+	assign divisor = 32'b011;
+	reg Clock;
+	wire done;
+	wire [5:0] count;
+	Alu_Div_32 DUT(dividend, divisor, Clock, done, count, A, Q);
+	
+	initial
+		 begin 
+			// Run clock
+			Clock = 0;
+			forever #10 Clock = ~ Clock;
+	end
+
+
+endmodule
