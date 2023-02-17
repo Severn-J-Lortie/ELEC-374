431d8b675264ca896e60329e5f279e7f9924d920
Severn Lortie <sevysdayoff@gmail.com>
2023-02-17 05:35:44 -0500

    Added SHRA tb

--- Alu_Shift_Right_Arith_32.v
+++ Alu_Shift_Right_Arith_32.v
@@ -1,5 +1,5 @@
 module Alu_Shift_Right_Arith_32(input [31:0] X, input [31:0] shift, output [31:0] Y);
 	wire [31:0] temp;
-	assign temp = 32'HFFFFFFFF;
+	assign temp = X[31] == 1 ? 32'HFFFFFFFF : 32'b0;
 	assign Y = (X >> shift) | (temp << (32 - shift)); // Basically a ROR with the top bits cloned
 endmodule
